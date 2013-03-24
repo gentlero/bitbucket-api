15135b44c02b092f10db8d5f7e80ef3f32e3fa70
Alexandru G <alex@gentle.ro>
2013-03-24 16:38:27 +0200

    created repo/links skeleton ; added links::all method

---
+++ lib/Gentle/Bitbucket/API/Repo/Links.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the bitbucket-api package.
+ *
+ * (c) Alexandru G. <alex@gentle.ro>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gentle\Bitbucket\API\Repo;
+
+use Gentle\Bitbucket\API;
+
+/**
+ * Links class
+ *
+ * Links connect your commit messages and code comments directly to your
+ * JIRA issue tracker or Bamboo build server.
+ *
+ * @author  Alexandru G.    <alex@gentle.ro>
+ */
+class Links extends API\Api
+{
+    /**
+     * Get list of links
+     *
+     * @access public
+     * @param  string $account The team or individual account owning the repository.
+     * @param  string $repo    The repository identifier.
+     * @return mixed
+     */
+    public function all($account, $repo)
+    {
+        return $this->requestGet(
+            sprintf('repositories/%s/%s/links', $account, $repo)
+        );
+    }
+}
---
+++ test/Gentle/Bitbucket/Tests/API/Repo/LinksTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Gentle\Bitbucket\Tests\API\Repo;
+
+use Gentle\Bitbucket\Tests\API as Tests;
+use Gentle\Bitbucket\API;
+
+class LinksTest extends Tests\TestCase
+{
+    public function testGetAllLinks()
+    {
+        $endpoint       = 'repositories/gentle/eof/links';
+        $expectedResult = json_encode('dummy');
+
+        $links = $this->getApiMock('Gentle\Bitbucket\API\Repo\Links');
+        $links->expects($this->once())
+            ->method('requestGet')
+            ->with($endpoint)
+            ->will( $this->returnValue($expectedResult) );
+
+        /** @var $links \Gentle\Bitbucket\API\Repo\Links */
+        $actual = $links->all('gentle', 'eof');
+
+        $this->assertEquals($expectedResult, $actual);
+    }
+}
