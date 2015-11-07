c17f1416d4806083818a07f573d5b4b6df1d746c
Alexandru Guzinschi <alex@gentle.ro>
2015-11-07 15:31:01 +0200

    version bumped to 0.7.1

--- lib/Bitbucket/API/Http/Client.php
+++ lib/Bitbucket/API/Http/Client.php
@@ -33,7 +33,7 @@ class Client extends ClientListener implements ClientInterface
         'api_versions'  => array('1.0', '2.0'),     // supported versions
         'format'        => 'json',
         'formats'       => array('json', 'xml'),    // supported response formats
-        'user_agent'    => 'bitbucket-api-php/0.7.0 (https://bitbucket.org/gentlero/bitbucket-api)',
+        'user_agent'    => 'bitbucket-api-php/0.7.1 (https://bitbucket.org/gentlero/bitbucket-api)',
         'timeout'       => 10,
         'verify_peer'   => false
     );
